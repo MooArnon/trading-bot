7613a6b36a45c83b307e2378d4618cd1186600e7
MooArnon <oomarnon.000@gmail.com>
2025-12-13 07:48:02 +0700

    [secure-binance-key]

--- config/config.py
+++ config/config.py
@@ -1,8 +1,13 @@
 import os
+from flow.utils import get_secret
+
 config = {}
 
-config['BINANCE_API_KEY'] = os.environ['BINANCE_API_KEY']
-config['BINANCE_SECRET_KEY'] = os.environ['BINANCE_SECRET_KEY']
-config['LEVERAGE'] = int(
-    os.environ["LEVERAGE"]
-)
+trading_bot_secrets = get_secret("trading-bot")
+secrets_dict = eval(trading_bot_secrets)
+os.environ["BINANCE_API_KEY"] = secrets_dict['BINANCE_API_KEY']
+os.environ["BINANCE_SECRET_KEY"] = secrets_dict['BINANCE_SECRET_KEY']
+
+config['BINANCE_API_KEY'] = secrets_dict['BINANCE_API_KEY']
+config['BINANCE_SECRET_KEY'] = secrets_dict['BINANCE_SECRET_KEY']
+config['LEVERAGE'] = int(os.environ["LEVERAGE"])
